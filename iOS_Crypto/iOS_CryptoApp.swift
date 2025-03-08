980549b5f90b2885df833496ad27aa967f9525dc
Ortega <ivanort98@gmail.com>
2025-03-08 15:13:48 -0600

    CPT-1: Initial project configuration

---
+++ iOS_Crypto/iOS_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CryptoApp.swift
+//  iOS_Crypto
+//
+//  Created by Ortega on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
